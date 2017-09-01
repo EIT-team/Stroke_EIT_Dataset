aecca9fe52d9f9d7f041dde575cd21f9fb62c338
Jimbles <j.avery.10@ucl.ac.uk>
2017-09-01 13:09:39 +0100

    Added code to recreate Averaged healty dataset. fixed bug in normalise_dataset

---
+++ resources/make_average_healthy.m
@@ -0,0 +1,38 @@
+%% Process Subjects
+
+% find files in the Patient directory
+fld_subject = dir('..\Subjects\Subject*');
+
+%take only the directories
+fld_subject=fld_subject([fld_subject.isdir]);
+
+%best 10 recordings - through observation of the SNR
+good_recs = {'01b','02a','03b','04a','05a','06a','07a','08a','09a','10a'};
+good_frame=[3,2,2,2,2,2,2,2,1,1];
+
+fld_subject=fld_subject(ismember({fld_subject.name}.',strcat('Subject_',good_recs)));
+
+% number of folders to process
+fnum_subject = size(fld_subject,1);
+
+%process all data in each folder, catching errors as we go
+for iSubject = 1:fnum_subject
+    try
+        dirname=dir([fld_subject(iSubject).folder filesep fld_subject(iSubject).name filesep '*MF*-BV.mat']);
+        BVcur=normalise_dataset([dirname.folder filesep dirname.name]);
+        
+        if iSubject ==1
+            BVtotal=nan(size(BVcur,1),size(BVcur,2),fnum_subject);
+        end
+        
+        BVtotal(:,:,iSubject)=squeeze(BVcur(:,:,good_frame(iSubject)));
+    catch err
+        fprintf(2, '%s\n', getReport(err, 'extended'));
+        fprintf(2, ' Error in processing folder %s\n',fld_subject(iSubject).name);
+    end
+    
+end
+
+Chn_ave=mean(BVtotal,3);
+
+save('Average_Healthy.mat','Chn_ave');
--- src/normalise_dataset.m
+++ src/normalise_dataset.m
@@ -3,40 +3,43 @@ function [ BV_corrected ] = normalise_dataset( IN )
 %   Detailed explanation goes here
 
 
-if ischar(IN)
+%% check input is path or structure
 
+if ischar(IN)
     BVStruc =load(IN);
-    
 elseif isstruct(IN)
     BVStruc=IN;
 end
 
 
+%% Load important variables
+Amplitudes = BVStruc.ExpSetup.Amp; % amplitude in uA
+Freqs=BVStruc.ExpSetup.Freq; % freq in Hz
+BV= BVStruc.BV; % boundary voltage magnitude
+PhaseAngle=BVStruc.PhaseAngle; % phase angle in rad
+keep_idx=BVStruc.keep_idx; % indicies of non injecting channels
+repeats=BVStruc.ExpSetup.Repeats; % number of times protocol repeated
 
-%% load BV, keep_idx, PhaseAngle, ExpSetup, prt_full
-Amplitudes = BVStruc.ExpSetup.Amp;
-Freqs=BVStruc.ExpSetup.Freq;
-BV= BVStruc.BV;
-PhaseAngle=BVStruc.PhaseAngle;
-keep_idx=BVStruc.keep_idx;
-
-% looad biosemi gain 
-load([mfilename('fullpath') '\..\..\resources\BioSemi_correction.mat']); 
-%% Correct for 
+% looad biosemi gain
+load([mfilename('fullpath') '\..\..\resources\BioSemi_correction.mat']);
+%% Correct for
 
 BV_corrected= NaN(size(keep_idx,2),size(BV,2),size(BV{1},2));
 
 % selecting non-injecting channels, converting to mV, computing Real compnent,normalizing for I, correcting for biosemi gain
-for iFrame= 1:size(BV{1},2) % for each frame (2-3)
-    BV_frame= NaN(size(keep_idx,2),size(BV,2));
+for iFrame= 1:repeats % for each frame (2-3)
+    BV_frame= nan(size(keep_idx,2),size(BV,2));
     for iFreq= 1:size(BV,2) % for each frequency (17)
         
-        curFreq=Freqs(iFreq);
-        curGain=BioSemi_correction_gain(BioSemi_correction_freq == curFreq);
-        
-        BV_frame(:,iFreq)= (BV{iFreq}(keep_idx,iFrame)./1000).*cos(PhaseAngle{iFreq}(keep_idx,iFrame)); %convert to real mV
-        BV_frame(:,iFreq)= BV_frame(:,iFreq)/Amplitudes(iFreq)*max(Amplitudes); % adjust for different current amplitudes
-        BV_frame(:,iFreq)= BV_frame(:,iFreq)*curGain; % adjust for biosemi gain
+        if (size(BV{iFreq},2) >= iFrame) % only do this if there is data for this frame
+
+            curFreq=Freqs(iFreq);
+            curGain=BioSemi_correction_gain(BioSemi_correction_freq == curFreq);
+            
+            BV_frame(:,iFreq)= (BV{iFreq}(keep_idx,iFrame)./1000).*cos(PhaseAngle{iFreq}(keep_idx,iFrame)); %convert to real mV
+            BV_frame(:,iFreq)= BV_frame(:,iFreq)/Amplitudes(iFreq)*max(Amplitudes); % adjust for different current amplitudes
+            BV_frame(:,iFreq)= BV_frame(:,iFreq)*curGain; % adjust for biosemi gain
+        end
     end
     
     BV_corrected(:,:,iFrame)= BV_frame;
