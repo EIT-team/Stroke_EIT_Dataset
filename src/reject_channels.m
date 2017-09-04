f82f9caacbea9f2a7c1afc426de46731d36a97cc
Jimbles <j.avery.10@ucl.ac.uk>
2017-09-04 16:34:59 +0100

    added extraction of patient info from textfile

--- src/reject_channels.m
+++ src/reject_channels.m
@@ -1,8 +1,7 @@
 function [ BV_cleaned  ] = reject_channels( BVin )
 % [ BV_cleaned, Chn_removed  ] = reject_channels( BVin )
-% REJECT_CHANNELS Summary of this function goes here
-%   Detailed explanation goes here
-
+% REJECT_CHANNELS sets channels to nan based on chosen criteria. Currently
+% a deviation more than 20mV from a notional good baseline is used. 
 
 % get the reference dataset used to reject bad channels
 load([mfilename('fullpath') '\..\..\resources\Average_Healthy.mat']);
@@ -12,7 +11,7 @@ BV_cleaned= BVin;
 for iFreq= 1: size(BVin,2) % for each frequency (17)
     for comb= 1: size(BVin)
         if (abs(BVin(comb, iFreq)) > (abs(Chn_ave(comb, iFreq)) + 20)) % cleaning criteria 
-            BV_cleaned(comb, iFreq)= NaN;
+            BV_cleaned(comb, iFreq)= nan;
             data_counter= data_counter+1; 
         end
     end
