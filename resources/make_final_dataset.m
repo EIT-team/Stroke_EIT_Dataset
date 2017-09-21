32db81a823c25cce10ce451d8279694c5cea2627
Jimbles <j.avery.10@ucl.ac.uk>
2017-09-21 18:13:11 +0100

    started make final dataset

--- resources/make_final_dataset.m
+++ resources/make_final_dataset.m
@@ -18,3 +18,16 @@ end
 Patients_chosen=[6;9;11;12;15;16;17;18;19;20;23;24;25;26];
 
 PatientsNum=length(Patients_chosen);
+
+
+for iRec = 1:PatientsNum
+    EITDATA(iRec).NameTag
+    EITDATA(iRec).Classification
+    EITDATA(iRec).VoltagesCleaned
+    EITDATA(iRec).VoltagesFull
+    EITDATA(iRec).RemovedChannels
+    EITDATA(iRec).Diagnosis
+    EITDATA(iRec).StudyID
+    EITDATA(iRec).Comments
+
+end
