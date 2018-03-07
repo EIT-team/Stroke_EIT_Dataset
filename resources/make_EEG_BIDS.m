ff92f084a25f2360af840a54b85ed3788428e571
Jimbles <j.avery.10@ucl.ac.uk>
2018-03-07 21:06:02 +0000

    all EEG BIDS files

--- resources/make_EEG_BIDS.m
+++ resources/make_EEG_BIDS.m
@@ -32,20 +32,25 @@ fprintf(fid,'%s\t%s\t%s\t%s\t%s\t%s\n','particap','type','description','sampling
 for iElec = 1:Elecnum-1
     fprintf(fid,'%s\t%s\t%s\t%d\t%d\t%d\n',Elec_name{iElec},'EEG/EIT','electrode',16384,0,0);
 end
-    fprintf(fid,'%s\t%s\t%s\t%d\t%d\t%d\n','STATUS','TRIG','Digital Triggers See ScouseTom EIT system desc.',16384,0,0);
+fprintf(fid,'%s\t%s\t%s\t%d\t%d\t%d\n','STATUS','TRIG','Digital Triggers See ScouseTom EIT system desc.',16384,0,0);
 fclose(fid);
 
 %% write participants file
+load('PatientSubjectInfo');
 
-fid=fopen('participants.tsv','w+');
-fprintf(fid,'%s\t%s\t%s\t%s\t%s\t%s\n','name','type','description','sampling_rate','low_cutoff','high_cutoff');
-for iElec = 1:Elecnum-1
-    fprintf(fid,'%s\t%s\t%s\t%d\t%d\t%d\n',Elec_name{iElec},'EEG/EIT','electrode',16384,0,0);
+fid=fopen('..\Patients\participants.tsv','w+');
+fprintf(fid,'%s\t%s\t%s\t%s\n','participant id','age','sex','group');
+for iPatient = 1:length(PatientInfo.name)
+    fprintf(fid,'%s\t%s\t%s\t%s\n',PatientInfo.name{iPatient},'-','-',PatientInfo.classification{iPatient});
 end
-    fprintf(fid,'%s\t%s\t%s\t%d\t%d\t%d\n','STATUS','TRIG','Digital Triggers See ScouseTom EIT system desc.',16384,0,0);
 fclose(fid);
 
-
+fid=fopen('..\Subjects\participants.tsv','w+');
+fprintf(fid,'%s\t%s\t%s\t%s\n','participant id','age','sex','group');
+for iSubject = 1:length(SubjectInfo.name)
+    fprintf(fid,'%s\t%s\t%s\t%s\n',SubjectInfo.name{iSubject},'-','-',SubjectInfo.classification{iSubject});
+end
+fclose(fid);
 
 %% write data description files
 
@@ -70,7 +75,7 @@ fid = fopen(json_data_description.file, 'wt');
 fprintf(fid, json_data_description.json);
 fclose(fid);
 
-% Patients 
+% Patients
 data_description.Name = 'The UCLH Stroke EIT Dataset - Patients';
 data_description.DatasetDOI = '10.5281\zenodo.838176 and 10.5281\zenodo.838184';
 
@@ -91,5 +96,73 @@ json_data_description.file = '..\Anonymised_Radiology\data_description.json';
 fid = fopen(json_data_description.file, 'wt');
 fprintf(fid, json_data_description.json);
 fclose(fid);
-%% write eeg description files
+%% make eeg description files
+
+eeg_sidecar.TaskName = 'rest';
+eeg_sidecar.EEGSamplingFrequency=16384;
+eeg_sidecar.ManufacturersAmplifierModelName='BioSemi ActiveTwo';
+eeg_sidecar.ManufacturersCapModelName='EasyCap';
+eeg_sidecar.EEGChannelCount=32;
+eeg_sidecar.EOGChannelCount=0;
+eeg_sidecar.ECGChannelCount=0;
+eeg_sidecar.EMGChannelCount=0;
+eeg_sidecar.TriggerChannelCount=8;
+eeg_sidecar.PowerLineFrequency=50;
+eeg_sidecar.EEGReference='common';
+
+json_eeg_sidecar = jsonencode(eeg_sidecar);
+%% copy to folders
+% put this metadata in each folder
+
+fld_subject = dir('..\Subjects\Subject*');
+
+%take only the directories
+fld_subject=fld_subject([fld_subject.isdir]);
+fnum_subject = size(fld_subject,1);
+
+for iSubject = 1:fnum_subject
+    
+    cur_dir=[fld_subject(iSubject).folder filesep fld_subject(iSubject).name];
+    %copy and rename channels.tsv
+    copyfile('_channels.tsv',cur_dir);
+    movefile([cur_dir filesep '_channels.tsv'],[cur_dir filesep fld_subject(iSubject).name '_channels.tsv']);
+    %copy and rename electrodes
+    copyfile('_electrodes.tsv',cur_dir);
+    movefile([cur_dir filesep '_electrodes.tsv'],[cur_dir filesep fld_subject(iSubject).name '_electrodes.tsv']);
+    %write eeg file
+    fid = fopen([cur_dir filesep fld_subject(iSubject).name '_eeg.json'], 'wt');
+    fprintf(fid, json_eeg_sidecar);
+    fclose(fid);
+    
+end
+
+fld_patient = dir('..\Patients\Patient*');
+
+%take only the directories
+fld_patient=fld_patient([fld_patient.isdir]);
+fnum_patient = size(fld_patient,1);
+
+for iPatient = 1:fnum_patient
+    
+    cur_dir=[fld_patient(iPatient).folder filesep fld_patient(iPatient).name];
+    %copy and rename channels.tsv
+    copyfile('_channels.tsv',cur_dir);
+    movefile([cur_dir filesep '_channels.tsv'],[cur_dir filesep fld_patient(iPatient).name '_channels.tsv']);
+    %copy and rename electrodes
+    copyfile('_electrodes.tsv',cur_dir);
+    movefile([cur_dir filesep '_electrodes.tsv'],[cur_dir filesep fld_patient(iPatient).name '_electrodes.tsv']);
+    %write eeg file
+    fid = fopen([cur_dir filesep fld_patient(iPatient).name '_eeg.json'], 'wt');
+    fprintf(fid, json_eeg_sidecar);
+    fclose(fid);
+    
+end
+
+
+
+
+
+
+
+
 
